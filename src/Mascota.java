e80c051d001fbd0c558a139c7926d2a53e6e2766
grupo2 <grupo2@IES-Sampedro>
2022-04-05 19:47:37 +0200

    :monocle_face: saldra esto :S

--- src/Mascota.java
+++ src/Mascota.java
@@ -1,5 +1,15 @@
 public class Mascota {
+	
 	private String nombre;
+	private float potencia;
+	
+	public float getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(float potencia) {
+		this.potencia = potencia;
+	}
 
 	public Mascota(String nombre) {
 		this.nombre = nombre;
@@ -15,7 +25,7 @@ public class Mascota {
 	}
 
 	public void MandarMensaje() {
-		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+		System.out.println(" Soy  " + this.nombre + " y te voy a contar algo o no y tengo una potencia de "  + this.potencia);
 	}
 
 }
